83724cb77dcedd98a28a44a28195c2076bae2bd0
Austin Willoughby <arw8020@rit.edu>
2018-02-23 23:01:25 -0500

    Logo Canvas: Working to render logo in canvas for animation
    Logo is drawing, but pixelated, and improperly resizing.

--- js/frontpage.js
+++ js/frontpage.js
@@ -1,5 +1,37 @@
+(function () {
+  const LOGO_CANVAS_ASPECT_RATIO = 1.8;
+  const LOGO_MAX_WIDTH = 1100;
+  var logoCanvas = document.getElementById('logoCanvas');
+  var logoCtx = logoCanvas.getContext('2d');
 
+
+  logoCtx.canvas.height = logoCtx.canvas.width / LOGO_CANVAS_ASPECT_RATIO;
+
+  var logo = new Image();
+
+  logo.addEventListener('load', function () {
+    drawLogo(logo, logo.width / logo.height, logoCtx, LOGO_MAX_WIDTH)
+  }, false);
+
+  logo.src = '../img/Title.gif';
+})();
 
 document.getElementById("videoFrame").onload = function () {
   var rect = document.getElementById("videoFrame").getBoundingClientRect();
+}
+
+window.onresize = function () {
+  var logoCanvas = document.getElementById('logoCanvas');
+  var logoCtx = logoCanvas.getContext('2d');
+
+  logoCtx.canvas.height = logoCtx.canvas.width / LOGO_CANVAS_ASPECT_RATIO;
+}
+
+function drawLogo(image, aspectRatio, context, maxWidth) {
+
+  var width = Math.min(maxWidth, context.canvas.width);
+
+  context.drawImage(image, 0, 0, width, width / aspectRatio);
+
+  requestAnimationFrame(drawLogo(image, aspectRatio, context, maxWidth));
 }
